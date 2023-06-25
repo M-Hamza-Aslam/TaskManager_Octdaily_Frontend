6d873e2e410a01def75ae6f7a0144823e5e98255
hamza <hamza.prolink@gmail.com>
2023-06-26 01:51:41 +0500

    .

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
 // export const BACKEND_DOMAIN = "http://localhost:8080";
-export const BACKEND_DOMAIN = "https://elated-pinafore-goat.cyclic.app";
+export const BACKEND_DOMAIN = "https://gentle-tick-loincloth.cyclic.app";
